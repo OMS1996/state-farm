1135d9fc2ab015940085f7589d35ea63df93348e
Omar Hussein <omarmoh.said@gmail.com>
2023-11-18 01:29:04 -0500

    main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
 # Main FastAPI app
+from fastapi import FastAPI
+from app.router import router  # Adjust the import path based on your project structure
+
+app = FastAPI()
+
+# Include the router
+app.include_router(router)
