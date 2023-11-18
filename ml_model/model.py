3a4c13fbc7a116f934db658f508f1e27a58d72d4
Omar Hussein <omarmoh.said@gmail.com>
2023-11-18 00:02:10 -0500

    feat: Add script for loading and making predictions with the ML model
    This commit adds a script responsible for loading a pre-trained machine learning model (saved as a pickle file or similar format) and using it to make predictions. The script provides a convenient way to utilize the model for prediction tasks.

--- ml_model/model.py
+++ ml_model/model.py
@@ -1,1 +1,15 @@
 # Code for loading and running the ML model
+import pandas as pd
+import pickle
+
+# Load the model (assuming it's a pickle file)
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+# Prediction function
+def predict(model, data):
+    # Assuming the model is a logistic regression from statsmodels
+    predicted_probs = model.predict(data)
+    return predicted_probs
