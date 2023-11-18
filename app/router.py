680d8167be8e775b4fb0e18984babe2988ddf635
Omar Hussein <omarmoh.said@gmail.com>
2023-11-18 01:09:42 -0500

    updated the predict endpoint

--- app/router.py
+++ app/router.py
@@ -7,18 +7,21 @@ from dependencies import MODEL_PATH
 
 router = APIRouter()
 
-# Load your model (consider loading it at startup rather than on each call)
-# model path and model.pkl
-model = load_model(MODEL_PATH + "/model.pkl")
+# Load your model
+model = load_model(MODEL_PATH)
 
 @router.post("/predict", response_model=PredictionOutput)
 async def get_prediction(input_data: PredictionInput):
     try:
         # Preprocess data
-        processed_data = preprocess_data(input_data.dict())
+        processed_data = preprocess_data(input_data.features)  # Assuming input_data.features is a dictionary
         # Make prediction
         prediction = predict(model, processed_data)
         # Convert prediction to your desired format
-        return PredictionOutput(probability=prediction, predicted_class="your_class")
+        # Example: Extracting probability and determining class
+        # Adjust this based on your model's output and class determination logic
+        probability = prediction[0]
+        predicted_class = "customer_purchased" if probability > 0.5 else "customer_did_not_purchase"  # Example threshold
+        return PredictionOutput(probability=probability, predicted_class=predicted_class)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
